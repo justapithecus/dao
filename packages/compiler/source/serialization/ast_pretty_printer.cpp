6516d24c79c5cd2ad5ae4b53e57b1964dce7c1cb
Andrew Hu <andrew@harta.io>
2023-10-30 17:57:33 -0400

    feat(compiler): organize json and testing serialization code (#15)

---
+++ packages/compiler/source/serialization/ast_pretty_printer.cpp
@@ -0,0 +1,74 @@
+#include "ast_pretty_printer.hpp"
+
+namespace dao {
+
+  auto ast_pretty_printer::operator()(dao::identifier_expr const &expr) const
+    -> json {
+    return json{
+      {"type", "identifier_expression"},
+      {"value", expr.name},
+    };
+  }
+
+  auto ast_pretty_printer::operator()(dao::numeral_expr const &expr) const
+    -> json {
+    return json{
+      {"type", "numeral_expression"},
+      {"value", expr.val},
+    };
+  }
+
+  auto ast_pretty_printer::operator()(dao::binary_expr const &expr) const
+    -> json {
+    auto lhs{std::visit(*this, *(expr.lhs))};
+    json rhs{};
+    if (expr.rhs) {
+      rhs = std::visit(*this, *(expr.rhs));
+    } else {
+      rhs = nullptr;
+    }
+
+    return json{
+      {"type", "binary_expression"},
+      {
+        "value",
+        {
+          {"lhs", lhs},
+          {"rhs", rhs},
+          {"operand", std::string{expr.op}},
+        },
+      },
+    };
+  }
+
+  auto ast_pretty_printer::operator()(dao::function_proto const &expr) const
+    -> json {
+    return json{
+      {"type", "function_prototype"},
+      {
+        "value",
+        {
+          {"id", expr.id},
+          {"args", expr.args},
+        },
+      },
+    };
+  }
+
+  auto ast_pretty_printer::operator()(dao::function_def const &expr) const
+    -> json {
+    auto body{std::visit(*this, *(expr.body))};
+
+    return json{
+      {"type", "function_definition"},
+      {
+        "value",
+        {
+          {"prototype", expr.proto},
+          {"body", body},
+        },
+      },
+    };
+  }
+
+} // namespace dao
