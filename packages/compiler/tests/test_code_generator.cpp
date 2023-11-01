fcfc50622d36dea091ff54e57a9e931bc0895c0c
Andrew Hu <andrew@harta.io>
2023-11-01 13:24:21 -0400

    rest

--- packages/compiler/tests/test_code_generator.cpp
+++ packages/compiler/tests/test_code_generator.cpp
@@ -9,21 +9,19 @@ auto directory{Approvals::useApprovalsSubdirectory("golden-files")};
 auto main() -> int {
   "triple"_test = [] {
     auto constexpr source_fname{"examples/triple.dao"};
-    auto ast{dao::parse(dao::lex(source_fname))};
     auto code_generator{dao::llvm_ir_code_generator{source_fname}};
 
-    std::visit(code_generator, ast);
+    std::visit(code_generator, dao::parse(dao::lex(source_fname)));
     Approvals::verify(code_generator.dumps(),
       ApprovalTests::Options().fileOptions().withFileExtension(".ll"));
   };
 
-  // "fibonacci"_test = [] {
-  //   auto constexpr source_fname{"examples/fibonacci.dao"};
-  //   auto ast{dao::parse(dao::lex(source_fname))};
-  //   auto code_generator{dao::llvm_ir_code_generator{source_fname}};
+  "fibonacci"_test = [] {
+    auto constexpr source_fname{"examples/fibonacci.dao"};
+    auto code_generator{dao::llvm_ir_code_generator{source_fname}};
 
-  //   std::visit(code_generator, ast);
-  //   Approvals::verify(code_generator.dumps(),
-  //     ApprovalTests::Options().fileOptions().withFileExtension(".ll"));
-  // };
+    std::visit(code_generator, dao::parse(dao::lex(source_fname)));
+    Approvals::verify(code_generator.dumps(),
+      ApprovalTests::Options().fileOptions().withFileExtension(".ll"));
+  };
 }
