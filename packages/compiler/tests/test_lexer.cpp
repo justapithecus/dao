5273612e0359431cec6795ad127923ff39a86e4c
Andrew Hu <andrew@harta.io>
2023-11-01 21:37:57 -0400

    map out expects

--- packages/compiler/tests/test_lexer.cpp
+++ packages/compiler/tests/test_lexer.cpp
@@ -6,6 +6,11 @@
 auto directory = Approvals::useApprovalsSubdirectory("golden-files");
 
 auto main() -> int {
+  "hello_world"_test = [] {
+    auto lexemes{dao::lex("examples/hello_world.dao")};
+    Approvals::verifyAll("hello_world.dao", lexemes);
+  };
+
   "fibonacci"_test = [] {
     auto lexemes{dao::lex("examples/fibonacci.dao")};
     Approvals::verifyAll("fibonacci.dao", lexemes);
