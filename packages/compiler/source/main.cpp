2e9279aec3ac5ffe06e4960e9c8a0b142e67d2a2
Andrew Hu <andrew@harta.io>
2023-11-01 21:09:34 -0400

    feat(compiler): hello world example (#21)

--- packages/compiler/source/main.cpp
+++ packages/compiler/source/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+#include "codegen/llvm_ir_code_generator.hpp"
+#include "parser/lexer.hpp"
+#include "parser/parser.hpp"
+
 auto main() -> int {
   std::cout << "Hello world!\n";
+
+  auto constexpr source_fname{"examples/hello_world.dao"};
+
+  auto tokens{dao::lex(source_fname)};
+  auto ast{dao::parse(tokens)};
+  auto code_generator{dao::llvm_ir_code_generator{source_fname}};
+
+  code_generator.generate(ast);
 }
--- packages/compiler/tests/test_code_generator.cpp
+++ packages/compiler/tests/test_code_generator.cpp
@@ -16,6 +16,15 @@ auto main() -> int {
       ApprovalTests::Options().fileOptions().withFileExtension(".ll"));
   };
 
+  "hello_world"_test = [] {
+    auto constexpr source_fname{"examples/hello_world.dao"};
+    auto code_generator{dao::llvm_ir_code_generator{source_fname}};
+
+    std::visit(code_generator, dao::parse(dao::lex(source_fname)));
+    Approvals::verify(code_generator.dumps(),
+      ApprovalTests::Options().fileOptions().withFileExtension(".ll"));
+  };
+
   "fibonacci"_test = [] {
     auto constexpr source_fname{"examples/fibonacci.dao"};
     auto code_generator{dao::llvm_ir_code_generator{source_fname}};
