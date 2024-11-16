231090ccd57eb92c34186ad4691c5add43549e82
Andrew Hu <me@andrewhu.nyc>
2024-11-16 15:12:27 -0500

    try llvm

--- cmake/llvm.cmake
+++ cmake/llvm.cmake
@@ -1,11 +1,11 @@
-find_package(LLVM REQUIRED 16 CONFIG)
+find_package(LLVM REQUIRED 19 CONFIG)
 message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 
 add_definitions(${LLVM_DEFINITIONS})
 
 execute_process(
-  COMMAND llvm-config-16 --ldflags
+  COMMAND llvm-config-19 --ldflags
   OUTPUT_VARIABLE LLVM_LD_FLAGS
 )
 string(STRIP ${LLVM_LD_FLAGS}  LLVM_LD_FLAGS)
@@ -13,7 +13,7 @@ message(STATUS "LLVM_LD_FLAGS: ${LLVM_LD_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LLVM_LD_FLAGS}")
 
 execute_process(
-  COMMAND llvm-config-16 --libs core
+  COMMAND llvm-config-19 --libs core
   OUTPUT_VARIABLE LLVM_LIBS
 )
 string(STRIP ${LLVM_LIBS}  LLVM_LIBS)
