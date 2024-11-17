1e8f3ffd54e07e8386b31a152e24829fa0b5d4a1
Andrew Hu <me@andrewhu.nyc>
2024-11-17 10:12:01 -0500

    fixes

--- cmake/llvm.cmake
+++ cmake/llvm.cmake
@@ -1,11 +1,14 @@
-find_package(LLVM REQUIRED 19 CONFIG)
+set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${DEVBOX_PACKAGES_DIR}")
+find_package(
+  LLVM 19.1 REQUIRED CONFIG
+)
 message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 
 add_definitions(${LLVM_DEFINITIONS})
 
 execute_process(
-  COMMAND llvm-config-19 --ldflags
+  COMMAND llvm-config --ldflags
   OUTPUT_VARIABLE LLVM_LD_FLAGS
 )
 string(STRIP ${LLVM_LD_FLAGS}  LLVM_LD_FLAGS)
@@ -13,7 +16,7 @@ message(STATUS "LLVM_LD_FLAGS: ${LLVM_LD_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LLVM_LD_FLAGS}")
 
 execute_process(
-  COMMAND llvm-config-19 --libs core
+  COMMAND llvm-config --libs core
   OUTPUT_VARIABLE LLVM_LIBS
 )
 string(STRIP ${LLVM_LIBS}  LLVM_LIBS)
